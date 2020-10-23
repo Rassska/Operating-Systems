bb82dc41e09953500234662fb751891af5bffc37
Rasul <rasul.yunusov282750@gmail.com>
2020-10-23 07:34:27 +0300

    new file X.log update log.sh

---
+++ ShellScripts/operations/log.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -e "/home/rasul/dev/OperatingSystems/ShellScripts/X.log" ]]; then # /var/log/anaconda/X.log
+	if [[ -r "/home/rasul/dev/OperatingSystems/ShellScripts/X.log" ]]; then # /var/log/anaconda/X.log
+		while read line
+		do
+			if [[ "$line" == *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]
+			then
+				echo -e ${line//"(WW)"/"\e[1;33mWarning:\e[0m"}
+			fi
+		done < "/home/rasul/dev/OperatingSystems/ShellScripts/X.log" # /var/log/anaconda/X.log
+
+		while read line
+		do
+			if [[ "$line" == *"(II)"* && "$line" != *"(++) from command line, (!!) notice, (II) informational,"* ]]
+			then
+				echo -e ${line//"(II)"/"\e[1;36mInformation:\e[0m"}
+			fi
+		done < "/home/rasul/dev/OperatingSystems/ShellScripts/X.log" # /var/log/anaconda/X.log
+	else
+		echo -ne "\e[1;41m Error: \e[0m" >&2
+		echo "There is no permission to read this file!" >&2
+		exit 3
+	fi
+else
+	echo -ne "\e[1;41m Error: \e[0m" >&2
+	echo "There is no such file or directory!" >&2
+	exit 3
+fi
