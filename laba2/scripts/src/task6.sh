28e13c86f8822b21e1ef549c5298214ba54aabef
Rasul <rasul.yunusov282750@gmail.com>
2020-12-21 09:27:19 +0300

    added task6.sh

---
+++ laba2/scripts/src/task6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in $(ps --no-headers -ef | awk '{print $2}') # i is proc
+do
+    res=$(grep "VmRSS" /proc/"$i"/status 2>/dev/null)
+    if [[ -n "$res" ]]; then
+        echo "$i $res"
+    fi
+done | sort -t ':' -r -n -k 2 | head -n 1 | awk '{print "/proc/ " $1}'
+
+top -o %MEM | head -n 8 | tail -n 1 | awk '{print "top " $2}'
