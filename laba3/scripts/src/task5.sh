94c5e98a8e4793ed0760850ff2f9c936ef97de26
Rasul <rasul.yunusov282750@gmail.com>
2020-12-24 10:29:35 +0300

    added task5.sh taskshelper.sh pipe line

---
+++ laba3/scripts/src/task5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+read string;
+echo "$string" > pipe
---
+++ laba3/scripts/src/task5helper.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+res=1
+mod="*"
+
+(tail -f pipe) |
+while true; 
+do
+    read string;
+    
+    case $string in 
+    QUIT)
+        echo "See you, man!"
+        killall tail
+        killall task5helper.sh
+        exit
+    ;;
+
+    "+")
+        mod="+"
+    ;;
+
+    "*")
+        mod="*"
+    ;;
+
+    [0-9]*)
+        
+        if [[ $mod == "+" ]]; then
+            let res=$res+$string
+            echo "Result was changed to $res"
+        else
+            let res=$res*$string
+            echo "Result was changed to $res"
+        fi
+    ;;
+
+    *)
+        echo "Wrong operation"
+        killall tail
+        killall task5helper.sh
+        exit
+    ;;
+
+    esac
+done
