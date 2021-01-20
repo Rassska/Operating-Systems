252016e0fce9b56e6a5ff299b65f4274f486e936
Rassska <rasul.yunusov282750@gmail.com>
2021-01-20 09:37:54 +0300

    init untrash.sh

---
+++ laba4/untrash.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+if !(( $# == 1 )); then
+    echo "Wrond amount of args!"
+    exit -1
+fi
+
+if [[ $1 == *"/"* ]]; then
+    echo "Only file anotherName, not the path"
+    exit -2
+fi
+
+
+[ -f $HOME/.trash.log ] || { exit -1; };
+[ -d "$HOME/.trash" ] || { exit -1; };
+[ -s $HOME/.trash.log ] || { exit -1; };
+
+
+cntFile=0
+
+for currFileToRef in $(cat $HOME/.trash.log)
+do
+    givenFilePath=$(echo $currFileToRef | awk '{ print $3 }')
+
+    if [[ $1 == "$(echo $givenFilePath | awk -F/ '{ print $NF }')" ]]; then
+        let cntFile=$cntFile+1
+        echo "Path: $givenFilePath"
+
+        read -p "(yes/no)?" currStatus
+        case $currStatus in
+        [yes]* )
+            receivingFile=$(echo $currFileToRef | awk '{ print $6 }')
+            currDiectory=$(echo $givenFilePath | awk 'BEGIN{ FS=OFS="/" }{ NF--; print }')
+            
+            if [ -d $currDiectory ]; then
+                if [ -f $givenFilePath ]; then
+                    read -p "This file exists, try another: " anotherName
+                    ln $HOME/.trash/$receivingFile $currDiectory/$anotherName
+
+                else
+                    ln $HOME/.trash/$receivingFile $givenFilePath
+                fi
+
+            else
+                if [ -f $HOME/$1 ]; then
+                    read -p "This file exists, try another: " anotherName
+                    ln $HOME/.trash/$receivingFile $HOME/$anotherName
+
+                else
+                    ln $HOME/.trash/$receivingFile $HOME/$1
+                fi
+
+            fi
+
+        grep -v "$receivingFile" $HOME/.trash.log > kek
+        mv kek /root/.trash.log
+        rm -rf $HOME/.trash/$receivingFile
+        ;;
+
+        [no]* )
+            ;;
+
+        * )
+            echo "there is no this command"
+            ;;
+        esac
+    fi
+done
