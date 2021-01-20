09c7bd6708ce3efb1d51c17f488e9a4165a45e18
Rassska <rasul.yunusov282750@gmail.com>
2021-01-20 07:10:26 +0300

    init rmtrash.sh

---
+++ laba4/rmtrash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if !(( $# == 1 )); then
+    echo "Wrond amount of args!"
+    exit -1
+fi
+
+if [[ ! -f $1 ]]; then
+    echo "Current file doesn't exist!"
+    exit -2
+fi
+
+mkdir $HOME/.trash;
+ln $1 $HOME/.trash/$RANDOM
+
+echo "deleted path: $PWD/$1 created link: $RANDOM" >> $HOME/.trash.log
+
+# vim .trash.log
+# rm -rf .trash.log
+# rm $1
